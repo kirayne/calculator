8afed3b7d77da26f8c782390c6050de95395dca5
kirayne <lbonifaciofreitas@gmail.com>
2021-06-22 18:33:39 -0300

    only 1 case left to finish, and adjust a bit of css, but the calculator is pretty much done

--- script.js
+++ script.js
@@ -2,83 +2,115 @@ let a = '';
 let b;
 let operator;
 let result;
-let elp;
-//getting the operators id string and storing inside operator
+//1st loop operator = id 2nd loop 0
 function operatorValue(e) {
-    operator = e.target.id;
+    if (operator === undefined) {
+        operator = e.target.id;
+    }
     stopA();
+    operator = e.target.id;
 }
-function stopA(){
-    if(b===undefined){
+
+function stopA() {
+    if (b === undefined) {
         b = +a;
         a = '';
     }
-    else{
+    else {
         a = +a;
-        operate (a, operator, b);
+        operate(a, operator, b);
+        a = '';
+        b = result;
+        document.querySelector('#display').textContent = result;
+        operator = undefined;
     }
 }
+
 //getting the number value and storing inside a
 function displayButton(e) {
-   value(e.target.value);
+    value(e.target.value);
 }
+
 //concatenating the input numbers
-function value(x){
+function value(x) {
     a += x;
+    document.querySelector('#display').textContent = a;
     console.log(a);
 }
+
 //reset the calculator
 function clear() {
+    a = '';
+    b = undefined;
+    operator = undefined;
+    result = undefined;
+    document.querySelector('#display').textContent = result;
+}
 
+function deleteLastNumber() {
+    a = document.querySelector('#display').textContent.toString().slice(0, -1);
+    document.querySelector('#display').textContent = a; 
 }
 
 function add(a, b) {
-    elp = a + b;
-    b = elp;
-    return elp;
+    return a + b;
 }
 
 function subtract(a, b) {
-    elp = b - a;
-    b = elp;
-    return elp;
+    return b - a;
 }
 
 function multiply(a, b) {
-    elp = a * b;
-    b = elp;
-    return elp;
+    return a * b;
 }
 
 function divide(a, b) {
-    elp = b / a;
-    b = elp;
-    return elp;
+    return b / a;
 }
+
 //receives a, b and the operator and call the functions
-function operate(a, operator, b) {
-    if (operator === 'add') {
+function operate(a, operators, b) {
+
+    if (operators === 'add') {
         result = add(a, b);
     }
-    else if (operator === 'subtract') {
+    else if (operators === 'subtract') {
         result = subtract(a, b);
     }
-    else if (operator === 'multiply') {
+    else if (operators === 'multiply') {
         result = multiply(a, b);
     }
     else {
         result = divide(a, b);
     }
+
     console.log(result);
 }
 
+function displayResult() {
+    a = +a;
+    operate(a, operator, b);
+    document.querySelector('#display').textContent = result;
+    b = result;
+}
+
+
+
 function loader() {
 
+    const del = document.querySelector('#del');
+    const cleaner = document.querySelector('#clear');
     const operators = document.querySelectorAll('.operators');
     const numbers = document.querySelectorAll('.numbers');
+    const equal = document.querySelector('#operate');
+
+    del.addEventListener('click', deleteLastNumber);
+    cleaner.addEventListener('click', clear);
+    equal.addEventListener('click', displayResult);
 
     operators.forEach(element =>
         element.addEventListener('click', operatorValue));
     numbers.forEach(element =>
         element.addEventListener('click', displayButton));
+
 }
